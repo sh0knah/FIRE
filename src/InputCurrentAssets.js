1a0cf69995ba5d84c0f53a17c8e15893d4b73675
Jason Caldwell <jason@avii.com>
2021-09-28 23:58:02 -0600

    Multi-column grid layout

--- src/InputCurrentAssets.js
+++ src/InputCurrentAssets.js
@@ -49,28 +49,30 @@ function InputCurrentAssets() {
 
     return (
         <div id="InputCurrentAssets" className="Input">
-            <label htmlFor="taxableStocks" className="Field-label">Taxable Stocks: </label><input type="number" onChange={e => setState({ ...state, taxableStocks: e.target.value })} className="Field-value Currency" id="TaxableStocks" value={state.taxableStocks} />
-            <label htmlFor="taxableBonds" className="Field-label">Taxable Bonds: </label><input type="number" onChange={e => setState({ ...state, taxableBonds: e.target.value })} className="Field-value Currency" id="TaxableBonds" value={state.taxableBonds} />
-            <label htmlFor="taxableCash" className="Field-label">Taxable Cash: </label><input type="number" onChange={e => setState({ ...state, taxableCash: e.target.value })} className="Field-value Currency" id="TaxableCash" value={state.taxableCash} />
-            <label htmlFor="taxableTotal" className="Field-label">Taxable Total: </label><div id="taxableTotal" className="Currency-total">{ totals.taxableTotal }</div>
+            <label htmlFor="taxableStocks" className="Field-label G1">Taxable Stocks: </label><input type="number" onChange={e => setState({ ...state, taxableStocks: e.target.value })} className="Field-value Currency G2" id="TaxableStocks" value={state.taxableStocks} />
+            <label htmlFor="rothStocks" className="Field-label G3">Roth Stocks: </label><input type="number" onChange={e => setState({ ...state, rothStocks: e.target.value })} className="Field-value Currency G4" id="RothStocks" value={state.rothStocks} />
+
+            <label htmlFor="taxableBonds" className="Field-label G1">Taxable Bonds: </label><input type="number" onChange={e => setState({ ...state, taxableBonds: e.target.value })} className="Field-value Currency G2" id="TaxableBonds" value={state.taxableBonds} />
+            <label htmlFor="rothBonds" className="Field-label G3">Roth Bonds: </label><input type="number" onChange={e => setState({ ...state, rothBonds: e.target.value })} className="Field-value Currency G4" id="RothBonds" value={state.rothBonds} />
+
+            <label htmlFor="taxableCash" className="Field-label G1">Taxable Cash: </label><input type="number" onChange={e => setState({ ...state, taxableCash: e.target.value })} className="Field-value Currency G2" id="TaxableCash" value={state.taxableCash} />
+            <label htmlFor="rothCash" className="Field-label G3">Roth Cash: </label><input type="number" onChange={e => setState({ ...state, rothCash: e.target.value })} className="Field-value Currency G4" id="RothCash" value={state.rothCash} />
+
+            <label htmlFor="taxableTotal" className="Field-label G1">Taxable Total: </label><div id="taxableTotal" className="Currency-total">{totals.taxableTotal}</div>
+            <label htmlFor="rothTotal" className="Field-label G3">Roth Total: </label><div id="rothTotal" className="Currency-total">{totals.rothTotal}</div>
 
             <div className="Spacer"></div>
 
-            <label htmlFor="rothStocks" className="Field-label">Roth Stocks: </label><input type="number" onChange={e => setState({ ...state, rothStocks: e.target.value })} className="Field-value Currency" id="RothStocks" value={state.rothStocks} />
-            <label htmlFor="rothBonds" className="Field-label">Roth Bonds: </label><input type="number" onChange={e => setState({ ...state, rothBonds: e.target.value })} className="Field-value Currency" id="RothBonds" value={state.rothBonds} />
-            <label htmlFor="rothCash" className="Field-label">Roth Cash: </label><input type="number" onChange={e => setState({ ...state, rothCash: e.target.value })} className="Field-value Currency" id="RothCash" value={state.rothCash} />
-            <label htmlFor="rothTotal" className="Field-label">Roth Total: </label><div id="rothTotal" className="Currency-total">{totals.rothTotal }</div>
+            <label htmlFor="taxDeferredStocks" className="Field-label G1">Tax Deferred Stocks: </label><input type="number" onChange={e => setState({ ...state, taxDeferredStocks: e.target.value })} className="Field-value Currency G2" id="Tax DeferredStocks" value={state.taxDeferredStocks} />
+            <label htmlFor="taxDeferredBonds" className="Field-label G1">Tax Deferred Bonds: </label><input type="number" onChange={e => setState({ ...state, taxDeferredBonds: e.target.value })} className="Field-value Currency G2" id="Tax DeferredBonds" value={state.taxDeferredBonds} />
+            <label htmlFor="taxDeferredCash" className="Field-label G1">Tax Deferred Cash: </label><input type="number" onChange={e => setState({ ...state, taxDeferredCash: e.target.value })} className="Field-value Currency G2" id="Tax DeferredCash" value={state.taxDeferredCash} />
+            <label htmlFor="taxDeferredTotal" className="Field-label G1">Tax Deferred Total: </label><div id="taxDeferredTotal" className="Currency-total">{totals.taxDeferredTotal }</div>
 
             <div className="Spacer"></div>
-
-            <label htmlFor="taxDeferredStocks" className="Field-label">Tax Deferred Stocks: </label><input type="number" onChange={e => setState({ ...state, taxDeferredStocks: e.target.value })} className="Field-value Currency" id="Tax DeferredStocks" value={state.taxDeferredStocks} />
-            <label htmlFor="taxDeferredBonds" className="Field-label">Tax Deferred Bonds: </label><input type="number" onChange={e => setState({ ...state, taxDeferredBonds: e.target.value })} className="Field-value Currency" id="Tax DeferredBonds" value={state.taxDeferredBonds} />
-            <label htmlFor="taxDeferredCash" className="Field-label">Tax Deferred Cash: </label><input type="number" onChange={e => setState({ ...state, taxDeferredCash: e.target.value })} className="Field-value Currency" id="Tax DeferredCash" value={state.taxDeferredCash} />
-            <label htmlFor="taxDeferredTotal" className="Field-label">Tax Deferred Total: </label><div id="taxDeferredTotal" className="Currency-total">{totals.taxDeferredTotal }</div>
-
+            <div className="Spacer"></div>
             <div className="Spacer"></div>
 
-            <label htmlFor="portfolioTotal" className="Field-label">Portfolio Total: </label><div id="portfolioTotal" className="Currency-total">{totals.portfolioTotal }</div>
+            <label htmlFor="portfolioTotal" className="Field-label G1">Portfolio Total: </label><div id="portfolioTotal" className="Currency-total G2">{totals.portfolioTotal }</div>
 
         </div>
     )
--- src/InputPersonal.js
+++ src/InputPersonal.js
@@ -30,17 +30,17 @@ function InputPersonal() {
 
     return (
         <div id="InputPersonal" className="Input">
-            <label htmlFor="Name" className="Field-label">Name: </label><input type="text" onChange={e => setState({ ...state, name: e.target.value }) } className="Field-value Text-long" id="Name" value={state.name} />
-            <label htmlFor="DOB" className="Field-label">Date of birth: </label><input type="date" onChange={e => setState({ ...state, dob: e.target.value })} className="Field-value Date" id="DOB" value={state.dob} />
-            <label htmlFor="REGoal" className="Field-label">Retirement goal date: </label><input type="date" onChange={e => setState({ ...state, regoal: e.target.value })} className="Field-value Date" id="REGoal" value={state.regoal} />
-            <label htmlFor="LifeExpectancy" className="Field-label">Life Expectancy: </label><input type="number" onChange={e => setState({ ...state, lifeExpectancy: e.target.value })} className="Field-value Number-short" id="LifeExpectancy" value={state.lifeExpectancy} />
+            <label htmlFor="Name" className="Field-label G1">Name: </label><input type="text" onChange={e => setState({ ...state, name: e.target.value }) } className="Field-value Text-long G2" id="Name" value={state.name} />
+            <label htmlFor="Partner" className="Field-label G3">Partner: </label><input type="text" onChange={e => setState({ ...state, partner: e.target.value })} className="Field-value Text-long G4" id="Partner" value={state.partner} />
 
-            <div className="Spacer"></div>
+            <label htmlFor="DOB" className="Field-label G1">Date of birth: </label><input type="date" onChange={e => setState({ ...state, dob: e.target.value })} className="Field-value Date G2" id="DOB" value={state.dob} />
+            <label htmlFor="DOB_Parter" className="Field-label G3">Date of birth: </label><input type="date" onChange={e => setState({ ...state, dob_Partner: e.target.value })} className="Field-value Date G4" id="DOB_Partner" value={state.dob_Partner} />
 
-            <label htmlFor="Partner" className="Field-label">Partner: </label><input type="text" onChange={e => setState({ ...state, partner: e.target.value })} className="Field-value Text-long" id="Partner" value={state.partner} />
-            <label htmlFor="DOB_Parter" className="Field-label">Date of birth: </label><input type="date" onChange={e => setState({ ...state, dob_Partner: e.target.value })} className="Field-value Date" id="DOB_Partner" value={state.dob_Partner} />
-            <label htmlFor="REGoal_Partner" className="Field-label">Retirement goal date: </label><input type="date" onChange={e => setState({ ...state, regoal_Partner: e.target.value })} className="Field-value Date" id="REGoal_Partner" value={state.regoal_Partner} />
-            <label htmlFor="LifeExpectancy_Partner" className="Field-label">Life Expectancy: </label><input type="number" onChange={e => setState({ ...state, lifeExpectancy_Partner: e.target.value })} className="Field-value Number-short" id="LifeExpectancy_Partner" value={state.lifeExpectancy_Partner} />
+            <label htmlFor="REGoal" className="Field-label G1">Retirement goal date: </label><input type="date" onChange={e => setState({ ...state, regoal: e.target.value })} className="Field-value Date G2" id="REGoal" value={state.regoal} />
+            <label htmlFor="REGoal_Partner" className="Field-label G3">Retirement goal date: </label><input type="date" onChange={e => setState({ ...state, regoal_Partner: e.target.value })} className="Field-value Date G4" id="REGoal_Partner" value={state.regoal_Partner} />
+
+            <label htmlFor="LifeExpectancy" className="Field-label G1">Life Expectancy: </label><input type="number" onChange={e => setState({ ...state, lifeExpectancy: e.target.value })} className="Field-value Number-short G2" id="LifeExpectancy" value={state.lifeExpectancy} />
+            <label htmlFor="LifeExpectancy_Partner" className="Field-label G3">Life Expectancy: </label><input type="number" onChange={e => setState({ ...state, lifeExpectancy_Partner: e.target.value })} className="Field-value Number-short G4" id="LifeExpectancy_Partner" value={state.lifeExpectancy_Partner} />
         </div>
     )
 }
